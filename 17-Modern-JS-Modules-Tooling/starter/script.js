89ea4ef1bf46755d6355c1272b4cd05f866933a6
by_biska <varakinkirill3@gmail.com>
2022-05-08 16:01:54 +0300

    1

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,35 @@
+import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+console.log(price, tq);
+console.log('Importing module');
+addToCart('bread', 5);
+
+import * as ShoppingCart from './shoppingCart.js'
+
+ShoppingCart.addToCart('bread', 5);
+
+import add, {cart} from './shoppingCart.js'
+add('pizza', 2)
+add('POOP', 22)
+add('water', 25)
+
+// console.log(cart)
+
+// console.log('asdjfk');
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+// console.log(data);
+// console.log('asdjfk');
+
+const getLastPost = async function(){
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const data = await res.json();
+
+    return {titles: data.at(-1).title, text: data.at(-1).body}
+}
+const lastPost = getLastPost()
+console.log(lastPost);
+
+// lastPost.then(last => console.log(last))
+
+const lastPost2 = await getLastPost()
+console.log(lastPost2);
