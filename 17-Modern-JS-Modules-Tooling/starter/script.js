615287b3b83683438c0999ea444055de40a8abe9
Work <varakinkirill3@gmail.com>
2022-05-08 17:30:12 +0300

    2

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,35 +1,60 @@
-import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-console.log(price, tq);
-console.log('Importing module');
-addToCart('bread', 5);
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price, tq);
+// console.log('Importing module');
+// addToCart('bread', 5);
 
-import * as ShoppingCart from './shoppingCart.js'
+// import * as ShoppingCart from './shoppingCart.js'
 
-ShoppingCart.addToCart('bread', 5);
+// ShoppingCart.addToCart('bread', 5);
 
-import add, {cart} from './shoppingCart.js'
-add('pizza', 2)
-add('POOP', 22)
-add('water', 25)
+// import add, {cart} from './shoppingCart.js'
+// add('pizza', 2)
+// add('POOP', 22)
+// add('water', 25)
 
-// console.log(cart)
+// // console.log(cart)
 
-// console.log('asdjfk');
-// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-// const data = await res.json();
-// console.log(data);
-// console.log('asdjfk');
+// // console.log('asdjfk');
+// // const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// // const data = await res.json();
+// // console.log(data);
+// // console.log('asdjfk');
 
-const getLastPost = async function(){
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    const data = await res.json();
+// const getLastPost = async function(){
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//     const data = await res.json();
 
-    return {titles: data.at(-1).title, text: data.at(-1).body}
-}
-const lastPost = getLastPost()
-console.log(lastPost);
+//     return {titles: data.at(-1).title, text: data.at(-1).body}
+// }
+// const lastPost = getLastPost()
+// console.log(lastPost);
 
-// lastPost.then(last => console.log(last))
+// // lastPost.then(last => console.log(last))
 
-const lastPost2 = await getLastPost()
-console.log(lastPost2);
+// const lastPost2 = await getLastPost()
+// console.log(lastPost2);
+
+const ShopingCart2 = (function () {
+    const cart = [];
+    const shoppingCost = 10
+    const totalPrice = 237;
+    const totalQuantity = 23;
+
+    const addToCart = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} added to cart`);
+    };
+
+    const orderStock = function (product, quantity) {
+        console.log(`${quantity} ${product} ordered from supplier`);
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    }
+})()
+
+ShopingCart2.addToCart('apple', 3)
